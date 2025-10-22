139b20065a0025f9f5b7dc237890e51613a0d047
Ziyad Bekrar <ziyad94240@gmail.com>
2025-10-22 18:10:48 +0200

    Update tables for prod

--- script_ad_spend/main.py
+++ script_ad_spend/main.py
@@ -71,17 +71,17 @@ def main(argv: List[str]) -> int:
     channels = {
         "tiktok": {
             "ids_map": TIKTOK_IDS,
-            "table": "TIA",
+            "table": "TiktokAds",
             "fetch": tiktok_spend_by_country_by_adgroup,
         },
         "meta": {
             "ids_map": META_IDS,
-            "table": "TIA",
+            "table": "MetaAds",
             "fetch": meta_spend_by_country,
         },
         "asa": {
             "ids_map": ASA_IDS,
-            "table": "TIA",
+            "table": "AsaAds",
             "fetch": asa_spend_by_country_by_adgroup,
         },
     }
